04aa3367d7b46d30554426be0bff5c66fcdf9b85
Erick Andres <e.andres4@yahoo.com>
2019-02-21 03:09:19 +0000

    Search to get twitterhandles

--- js/containers/ActionForm.jsx
+++ js/containers/ActionForm.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { getQueryVariables } from '../utils';
 import { CONF, URLS } from '../config';
-
+import jsonData from "../../legislators-current.json";
+import legislators_media from "../../legislators-social-media.json";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { keys } from '../config/keys.js'
 
 class ActionForm extends Component {
 
@@ -10,20 +12,66 @@ class ActionForm extends Component {
         super(props);
         this.state = getQueryVariables();
         this.state.sent = false;
-      
+        
+        this.state.zipcode = '';
+        this.state.usstate = '';
+        this.state.twitterHandles = [];
+        this.state.congressMembersID = [];
+        
+        this.handleChange = this.handleChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.click = this.click.bind(this);
+        this.lookUpStateByZipCode = this.lookUpStateByZipCode.bind(this);
+        this.lookUpTwitterHandles = this.lookUpTwitterHandles.bind(this);
+    }
+
+    lookUpTwitterHandles() {
+      const copyMembersID = this.state.congressMembersID.slice(); 
+      for(var i = 0; i < copyMembersID.length; i++) {
+        for (var j = 0; j < legislators_media.length; j++) {
+          var object = legislators_media[j];
+          var legislator_bioID = object.id.bioguide;
+          var twitterHandle = object.social.twitter;
+          if (legislator_bioID == copyMembersID[i]) {
+            this.setState({ twitterHandles: [...this.state.twitterHandles, twitterHandle]});
+          }
+        }
+      }
+    }
+
+    lookUpStateByZipCode(zipcode) {       
+      var url = "https://www.zipcodeapi.com/rest/"+keys.api_key+"/info.json/" + zipcode + "/radians";
+      $.ajax({
+        "url": url,
+        "dataType": "json"
+      }).done(function(data) {
+        this.setState({usstate: data.state});
+        for(var i = 0; i < jsonData.length; i++) {
+          var object = jsonData[i];
+          var legislator_bioID = object.id.bioguide; 
+          var objectTermsLst = object.terms;
+          var currentTerm = objectTermsLst[objectTermsLst.length - 1];
+          if (currentTerm.state == this.state.usstate) { 
+            this.setState({ congressMembersID: [...this.state.congressMembersID, legislator_bioID]});
+          }
+        }
+        this.lookUpTwitterHandles();
+      }.bind(this)).fail(function(data) {
+        console.log("Request failed");
+      });
+    }
+
+    handleChange(event) { 
+      this.setState({zipcode: event.target.value});
     }
     
     onSubmit(evt) {
       evt.preventDefault();
-      
       const fullname = document.getElementById('fullname'); 
       const email = document.getElementById('emailform'); 
-      console.log("PRINGINT INFORMATION");
-      console.log(fullname);
-      console.log(email);
-      const zip = document.getElementById('zip');   
+      const zip = document.getElementById('zip'); // dont think i need this part 
+      this.lookUpStateByZipCode(this.state.zipcode); // correct syntax 
+      // this.lookUpCongressByZipcode(this.state.zipcode);
 
       const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
       const nameRegex = /^[A-Za-z '.-]+$/.test(fullname.value);
@@ -121,11 +169,13 @@ class ActionForm extends Component {
       
       return(
         <form id="subForm" name="petition" className="js-cm-form">
-    			<input type="text" id="fullname" name="fullname" placeholder="full name" pattern="[A-Za-z '.-]+"/>
-    			<input type="email" id="emailform" name="email" required="" placeholder="email address"/>
-    			<input type="text" id="zip" name="zip" required="" placeholder="zip code"/>
-    			{button}
-    		</form>
+          <input type="text" id="fullname" name="fullname" placeholder="full name" pattern="[A-Za-z '.-]+"/>
+          <input type="email" id="emailform" name="email" required="" placeholder="email address"/>
+          <input type="text" id="zip" name="zip" required="" placeholder="zip code" value={this.state.zipcode}
+            onChange={this.handleChange}/>
+          <div className="text-error"></div> 
+          {button}
+        </form>
       );
     }  
   }
